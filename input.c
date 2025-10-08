c66c7e1d25a1f0aef0166719f7e21ca8c069b403
dragon <puneethans18@gmail.com>
2025-10-08 16:55:09 +0530

    input.c

--- input.c
+++ input.c
@@ -1,3 +1,34 @@
 #include<stdlib.h>
 #include<ncurses.h>
 #include"tetris.h"
+
+void input(tetris *tet)
+{
+	int ch = getch();
+	switch(ch) {
+		case RIGHT:
+			update_x(tet, 1);
+			break;
+		case LEFT:
+			update_x(tet, -1);
+			break;
+		case UP:
+			rotate_tetris(tet);
+			break;
+		case DOWN:
+			set_napms(100);
+			break;
+		case q:
+			GAME_STATUS = false;
+			break;
+		default:
+			/*
+			 * Do nothing
+			 */
+			break;
+	}
+	return;
+}
+
+		
+			
